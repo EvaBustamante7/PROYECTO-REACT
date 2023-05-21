94101b8472decf0453b0a8b64eaeecf4bf387f5b
Eva Bustamante <evabrodriguez7@gmail.com>
2023-05-21 19:56:46 +0200

    React App almost finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import Weather from './routes/Weather';
+import Gifs from './routes/Gifs';
+import Planets from './routes/Planets';
+import Contact from './routes/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/weather' element={<Weather/>} />
+        <Route path='/gifs' element={<Gifs/>} />
+        <Route path='/planets' element={<Planets/>} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../componentes/Navbar'
+import Footer from '../componentes/Footer'
+
+const Weather = () => {
+  return (
+    <div>
+        <Navbar />
+        <Footer />
+    </div>
+  )
+}
+
+export default Weather
