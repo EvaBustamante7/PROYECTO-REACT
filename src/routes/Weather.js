445fc8cf135379fabaae3e528cc48fddb5ae30fa
Eva Bustamante <evabrodriguez7@gmail.com>
2023-05-22 15:08:32 +0200

    Weather API

--- src/routes/Weather.js
+++ src/routes/Weather.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../componentes/Navbar'
 import Footer from '../componentes/Footer'
+import WeatherApi from '../componentes/WeatherApi'
 
 const Weather = () => {
   return (
     <div>
-        <Navbar />
+        
+        <WeatherApi />
         <Footer />
     </div>
   )
